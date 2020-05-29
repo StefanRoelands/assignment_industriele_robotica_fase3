2aed9198aeafff8741d8cd322eb0c235ec66125e
StefanRoelands <ag.roelands@student.avans.nl>
2020-05-29 12:19:21 +0200

    Positie toegevoegd + test programma uitgebreid

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_programma_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_programma_sm.py
@@ -48,7 +48,7 @@ class TestprogrammaSM(Behavior):
 
 
 	def create(self):
-		# x:1607 y:628, x:966 y:441
+		# x:385 y:685, x:966 y:441
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.config_name_gantry = 'Gantry_Home'
 		_state_machine.userdata.move_group_gantry = 'Gantry'
@@ -69,6 +69,7 @@ class TestprogrammaSM(Behavior):
 		_state_machine.userdata.rotation = 0
 		_state_machine.userdata.tool_link_right = 'right_ee_link'
 		_state_machine.userdata.arm_id = 'right'
+		_state_machine.userdata.config_name_PreDrop = 'Gantry_Predrop_AGV_1'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -172,14 +173,14 @@ class TestprogrammaSM(Behavior):
 			# x:1510 y:247
 			OperatableStateMachine.add('GripperOn',
 										VacuumGripperControlState(enable=True),
-										transitions={'continue': 'Move_right_arm_home_2', 'failed': 'failed', 'invalid_arm_id': 'failed'},
+										transitions={'continue': 'Move_right_arm_home_2', 'failed': 'ComputePick', 'invalid_arm_id': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'invalid_arm_id': Autonomy.Off},
 										remapping={'arm_id': 'arm_id'})
 
 			# x:1517 y:381
 			OperatableStateMachine.add('Move_right_arm_home_2',
 										SrdfStateToMoveitAriac(),
-										transitions={'reached': 'finished', 'planning_failed': 'Retry_7', 'control_failed': 'Retry_7', 'param_error': 'failed'},
+										transitions={'reached': 'PreSide_2', 'planning_failed': 'Retry_7', 'control_failed': 'Retry_7', 'param_error': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
 										remapping={'config_name': 'config_name_right_arm', 'move_group': 'move_group_right_arm', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
@@ -188,6 +189,45 @@ class TestprogrammaSM(Behavior):
 										WaitState(wait_time=0.3),
 										transitions={'done': 'Move_right_arm_home_2'},
 										autonomy={'done': Autonomy.Off})
+
+			# x:1525 y:496
+			OperatableStateMachine.add('PreSide_2',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'Move_gantry_home_2', 'planning_failed': 'Retry_8', 'control_failed': 'Retry_8', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_PreSide', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1523 y:593
+			OperatableStateMachine.add('Move_gantry_home_2',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'PreDrop_AGV1', 'planning_failed': 'Retry_9', 'control_failed': 'Retry_9', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_gantry', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1730 y:498
+			OperatableStateMachine.add('Retry_8',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'PreSide_2'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1551 y:733
+			OperatableStateMachine.add('Retry_9',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_gantry_home_2'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1334 y:727
+			OperatableStateMachine.add('Retry_10',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'PreDrop_AGV1'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1305 y:592
+			OperatableStateMachine.add('PreDrop_AGV1',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'Retry_10', 'control_failed': 'Retry_10', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_PreDrop', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
 
 		return _state_machine
