4e6755616500e37da3be2146805b661b2bb6ae92
StefanRoelands <ag.roelands@student.avans.nl>
2020-05-29 09:33:21 +0200

    Test behavior en GripperEnable.py

---
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_programma_sm.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
+from flexbe_states.wait_state import WaitState
+from ariac_flexbe_states.compute_grasp_ariac_state import ComputeGraspAriacState
+from ariac_flexbe_states.detect_part_camera_ariac_state import DetectPartCameraAriacState
+from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
+from ariac_flexbe_states.GripperEnable import VacuumGripperControlState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Tue May 26 2020
+@author: Stefan Roelands, Wouter de Saegher
+'''
+class TestprogrammaSM(Behavior):
+	'''
+	Testen van het oppakken
+	'''
+
+
+	def __init__(self):
+		super(TestprogrammaSM, self).__init__()
+		self.name = 'Test programma'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:1607 y:628, x:966 y:441
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.config_name_gantry = 'Gantry_Home'
+		_state_machine.userdata.move_group_gantry = 'Gantry'
+		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = ''
+		_state_machine.userdata.move_group_right_arm = 'Right_Arm'
+		_state_machine.userdata.config_name_left_arm = 'Left_Home'
+		_state_machine.userdata.config_name_right_arm = 'Right_Home'
+		_state_machine.userdata.move_group_left_arm = 'Left_Arm'
+		_state_machine.userdata.config_name_PreSide = 'Gantry_Pre_Side'
+		_state_machine.userdata.config_name_PreGrasp = 'Gantry_PreGrasp_Right_bins_1'
+		_state_machine.userdata.camera_ref_frame = 'world'
+		_state_machine.userdata.camera_topic = '/ariac/logical_camera_1'
+		_state_machine.userdata.camera_frame = 'logical_camera_1_frame'
+		_state_machine.userdata.part_type = 'pulley_part_red'
+		_state_machine.userdata.offset = 0.085
+		_state_machine.userdata.rotation = 0
+		_state_machine.userdata.tool_link_right = 'right_ee_link'
+		_state_machine.userdata.arm_id = 'right'
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:164 y:146
+			OperatableStateMachine.add('Move_left_arm_home',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'Move_right_arm_home', 'planning_failed': 'Retry', 'control_failed': 'Retry', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_left_arm', 'move_group': 'move_group_left_arm', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:201 y:38
+			OperatableStateMachine.add('Retry',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_left_arm_home'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:332 y:148
+			OperatableStateMachine.add('Move_right_arm_home',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'Move_gantry_home', 'planning_failed': 'Retry_2', 'control_failed': 'Retry_2', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_right_arm', 'move_group': 'move_group_right_arm', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:362 y:37
+			OperatableStateMachine.add('Retry_2',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_right_arm_home'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:525 y:149
+			OperatableStateMachine.add('Move_gantry_home',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'PreSide', 'planning_failed': 'Retry_3', 'control_failed': 'Retry_3', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_gantry', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:549 y:32
+			OperatableStateMachine.add('Retry_3',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_gantry_home'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:745 y:152
+			OperatableStateMachine.add('PreSide',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'DetectPart', 'planning_failed': 'Retry_4', 'control_failed': 'Retry_4', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_PreSide', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:773 y:27
+			OperatableStateMachine.add('Retry_4',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'PreSide'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1111 y:151
+			OperatableStateMachine.add('PreGrasp',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'ComputePick', 'planning_failed': 'Retry_5', 'control_failed': 'Retry_5', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_PreGrasp', 'move_group': 'move_group_gantry', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1129 y:33
+			OperatableStateMachine.add('Retry_5',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'PreGrasp'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1308 y:150
+			OperatableStateMachine.add('ComputePick',
+										ComputeGraspAriacState(joint_names=['right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_elbow_joint', 'right_wrist_1_joint', 'right_wrist_2_joint', 'right_wrist_3_joint']),
+										transitions={'continue': 'Move_to_pick', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'move_group': 'move_group_right_arm', 'move_group_prefix': 'move_group_prefix', 'tool_link': 'tool_link_right', 'pose': 'pose', 'offset': 'offset', 'rotation': 'rotation', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:918 y:149
+			OperatableStateMachine.add('DetectPart',
+										DetectPartCameraAriacState(time_out=0.5),
+										transitions={'continue': 'PreGrasp', 'failed': 'failed', 'not_found': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'ref_frame': 'camera_ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part_type', 'pose': 'pose'})
+
+			# x:1552 y:44
+			OperatableStateMachine.add('Retry_6',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_to_pick'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:1501 y:148
+			OperatableStateMachine.add('Move_to_pick',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'GripperOn', 'planning_failed': 'Retry_6', 'control_failed': 'GripperOn'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'move_group_prefix': 'move_group_prefix', 'move_group': 'move_group_right_arm', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1510 y:247
+			OperatableStateMachine.add('GripperOn',
+										VacuumGripperControlState(enable=True),
+										transitions={'continue': 'Move_right_arm_home_2', 'failed': 'failed', 'invalid_arm_id': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'invalid_arm_id': Autonomy.Off},
+										remapping={'arm_id': 'arm_id'})
+
+			# x:1517 y:381
+			OperatableStateMachine.add('Move_right_arm_home_2',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'Retry_7', 'control_failed': 'Retry_7', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_right_arm', 'move_group': 'move_group_right_arm', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:1729 y:398
+			OperatableStateMachine.add('Retry_7',
+										WaitState(wait_time=0.3),
+										transitions={'done': 'Move_right_arm_home_2'},
+										autonomy={'done': Autonomy.Off})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
---
+++ ariac_behaviors/ariac_flexbe_states/src/ariac_flexbe_states/GripperEnable.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+from flexbe_core import EventState, Logger
+import sys
+import rospy
+import rostopic
+from nist_gear.srv import VacuumGripperControl, VacuumGripperControlRequest, VacuumGripperControlResponse
+from nist_gear.msg import VacuumGripperState
+from std_msgs.msg import String
+
+class VacuumGripperControlState(EventState):
+	'''
+	Example for a state to demonstrate which functionality is available for state implementation.
+	This example lets the behavior wait until the given target_time has passed since the behavior has been started.
+
+	-- target_time 	float 	Time which needs to have passed since the behavior started.
+
+	<= continue 			Given time has passed.
+	<= failed 				Example for a failure outcome.
+
+	'''
+
+	def __init__(self, enable):
+		# Declare outcomes, input_keys, and output_keys by calling the super constructor with the corresponding arguments.
+		super(VacuumGripperControlState, self).__init__(input_keys = ['arm_id'], outcomes = ['continue', 'failed', 'invalid_arm_id'])
+
+		self._enable = enable
+
+		pass
+
+	def execute(self, userdata):
+		# This method is called periodically while the state is active.
+		# Main purpose is to check state conditions and trigger a corresponding outcome.
+		# If no outcome is returned, the state will stay active.
+
+
+		if userdata.arm_id == 'left':
+			gripper_service = '/ariac/gantry/left_arm/gripper/control'
+		else:
+			if userdata.arm_id == 'right':
+				gripper_service = '/ariac/gantry/right_arm/gripper/control'
+			else:
+				return 'invalid_arm_id'
+
+
+		rospy.loginfo("Waiting for service...")
+		rospy.wait_for_service(gripper_service) # use arm2 for controlling gripper on second manipulator
+		try:
+			# Create a service proxy.
+			vacuum_gripper_control = rospy.ServiceProxy(gripper_service, VacuumGripperControl)
+
+
+			request = VacuumGripperControlRequest()
+			request.enable = self._enable
+
+			# Call the service here.
+			service_response = vacuum_gripper_control(request)
+			rospy.loginfo("I only got here AFTER the service call was completed!")
+
+			# Return the response to the calling function.
+			if service_response.success == True:
+				if self._enable == True:
+					if userdata.arm_id == 'left':
+						status = rospy.wait_for_message('/ariac/gantry/left_arm/gripper/state', VacuumGripperState)
+						if status.attached == True:
+							return 'continue'
+					elif userdata.arm_id == 'right':
+						status = rospy.wait_for_message('/ariac/gantry/right_arm/gripper/state', VacuumGripperState)
+						if status.attached == True:
+							return 'continue'
+					else:
+						return 'failed'
+				else:
+					return 'continue'
+			else:
+				return 'failed'
+
+		except rospy.ServiceException, e:
+			rospy.loginfo( "Service call failed: %s"%e)
+			return 'failed' # One of the outcomes declared above.
+
+		pass
+		
+
+	def on_enter(self, userdata):
+		# This method is called when the state becomes active, i.e. a transition from another state to this one is taken.
+		# It is primarily used to start actions which are associated with this state.
+
+		# The following code is just for illustrating how the behavior logger works.
+		# Text logged by the behavior logger is sent to the operator and displayed in the GUI.
+
+		pass
+		''' time_to_wait = (self._target_time - (rospy.Time.now() - self._start_time)).to_sec()
+
+		if time_to_wait > 0:
+			Logger.loginfo('Need to wait for %.1f seconds.' % time_to_wait) '''
+
+
+	def on_exit(self, userdata):
+		# This method is called when an outcome is returned and another state gets active.
+		# It can be used to stop possibly running processes started by on_enter.
+
+		pass # Nothing to do in this example.
+
+
+	def on_start(self):
+		# This method is called when the behavior is started.
+		# If possible, it is generally better to initialize used resources in the constructor
+		# because if anything failed, the behavior would not even be started.
+
+		# In this example, we use this event to set the correct start time.
+		self._start_time = rospy.Time.now()
+
+
+	def on_stop(self):
+		# This method is called whenever the behavior stops execution, also if it is cancelled.
+		# Use this event to clean up things like claimed resources.
+
+		pass # Nothing to do in this example.
+		
